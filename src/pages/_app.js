83091d23707c03cdb3b8369a88e2a03c6d27ed9b
N0V@ <refunfk@gmail.com>
2024-09-07 18:12:08 +0530

    font setups

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import '@/styles/globals.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
